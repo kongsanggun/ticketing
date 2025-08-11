ebbdc222a8440940b3b9a3cd7561a40519198234
kongsanghyean <dodoi77@naver.com>
2025-08-11 12:30:29 +0900

    first commit

---
+++ src/main/java/ticket/test/ticketing/TicketingRequestDto.java
@@ -0,0 +1,62 @@
+package ticket.test.ticketing;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class TicketingRequestDto {
+
+    public String getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(String ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getShowId() {
+        return showId;
+    }
+
+    public void setShowId(String showId) {
+        this.showId = showId;
+    }
+
+    public String getSeat() {
+        return seat;
+    }
+
+    public void setSeat(String seat) {
+        this.seat = seat;
+    }
+
+    public Date getBookTime() {
+        return bookTime;
+    }
+
+    public void setBookTime(Date bookTime) {
+        this.bookTime = bookTime;
+    }
+
+    private String ticketId;
+    private String userId;
+    private String showId;
+    private String seat;
+    private Date bookTime;
+
+}
