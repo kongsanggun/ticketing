8bd5746c087a7e434793c4fdfbdf2bc9fc5550f5
kongsanghyean <dodoi77@naver.com>
2025-12-10 20:55:23 +0900

    Refactor : Refactor Code by conversation
    - change status in ExceptionCode
    - edit random value (new Random)
    
    PR #46

--- src/main/java/app/ticket/ticketing/common/exception/ExceptionCode.java
+++ src/main/java/app/ticket/ticketing/common/exception/ExceptionCode.java
@@ -7,8 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ExceptionCode {
-    SEAT_NOT_AVAILABLE("예약 가능한 자리가 존재하지 않습니다.", HttpStatus.NOT_FOUND),
-    SEAT_SELECTED("이미 선점된 자리입니다.", HttpStatus.FORBIDDEN),
+    SEAT_NOT_AVAILABLE("예약 가능한 자리가 존재하지 않습니다.", HttpStatus.BAD_REQUEST),
+    SEAT_SELECTED("이미 선점된 자리입니다.", HttpStatus.BAD_REQUEST),
     NOT_DATA("해당 값이 존재하지 않습니다.", HttpStatus.NOT_FOUND),
     ADDED_SHOW("이미 추가된 공연입니다.", HttpStatus.CONFLICT),
     EMPTY_PRICE("삭제 이후 공연 내 가격이 존재하지 않습니다.", HttpStatus.UNPROCESSABLE_ENTITY),
